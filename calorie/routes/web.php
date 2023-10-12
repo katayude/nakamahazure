6f818c374f4f92ddd17a53cb37b6cd54c7210cb3
yuyuuyuu <fujiyoshi.yusuke.671@s.kyushu-u.ac.jp>
2023-10-12 14:05:53 +0900

    カレンダーページの表示

--- calorie/routes/web.php
+++ calorie/routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CalendarController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,7 +31,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/calendar', [CalendarController::class, 'show'])->name('calorie.calendar');
+    Route::get('/calendar', [CalendarController::class, 'show'])->name('calendar');
     Route::get('/input', [FollowController::class, 'input'])->name('calorie.input');
 });
 
