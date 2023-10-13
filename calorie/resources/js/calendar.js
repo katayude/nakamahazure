0f3606abec0a15759c08a366b4de19113b4899e5
yuyuuyuu <fujiyoshi.yusuke.671@s.kyushu-u.ac.jp>
2023-10-13 14:29:08 +0900

    カレンダーページの移動アクション

--- calorie/resources/js/calendar.js
+++ calorie/resources/js/calendar.js
@@ -1,15 +1,24 @@
 import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction"; // インタラクションプラグインを追加
 
 var calendarEl = document.getElementById("calendar");
 
 let calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin],
+    plugins: [dayGridPlugin, interactionPlugin], // インタラクションプラグインを有効にする
     initialView: "dayGridMonth",
     headerToolbar: {
         left: "prev,next today",
         center: "title",
         right: "",
+    },
+    dateClick: function (info) {
+        // クリックした日付を取得
+        var clickedDate = info.date;
+        var formattedDate = clickedDate.toISOString().slice(0, 10); // 日付をYYYY-MM-DD形式にフォーマット
+
+        // ページをリダイレクト
+        window.location.href = '/your-redirect-url/' + formattedDate;
     },
 });
 calendar.render();
--- calorie/resources/views/calendar.blade.php
+++ calorie/resources/views/calendar.blade.php
@@ -1,8 +1,34 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta charset='utf-8' />
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/6.10.0/main.min.css' rel='stylesheet' />
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/6.10.0/main.min.js'></script>
+    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/interaction@6.10.0/main.js'></script>
+</head>
 <body>
     <div id='calendar'></div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                plugins: ['interaction'], // Interaction プラグインを有効にする
+
+                // カレンダーの設定...
+
+                dateClick: function(info) {
+                    // クリックした日付を取得
+                    var clickedDate = info.date;
+                    var formattedDate = clickedDate.toISOString().slice(0, 10); // 日付をYYYY-MM-DD形式にフォーマット
+
+                    // ページをリダイレクト
+                    window.location.href = 'http://localhost/dashboard/' + formattedDate;
+                },
+            });
+            calendar.render();
+        });
+    </script>
 </body>
 </html>
