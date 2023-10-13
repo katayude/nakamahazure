7b6a1a7a5656933aaddbb72ec3f31f9e8ebde370
skenta114 <s.kentafkm@icloud.com>
2023-10-13 22:37:13 +0900

    棒グラフ作った

--- calorie/resources/views/dashboard.blade.php
+++ calorie/resources/views/dashboard.blade.php
@@ -15,45 +15,39 @@
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js" type="text/javascript"></script>
                     <canvas id="graph-area" height="450" width="600"></canvas>
                     <script type="text/javascript">
+                        $protein = 300;
+                        $lipid = 20;
+                        $carbohydrates = 500;
+                        $in_protein = 450;
+                        $in_lipid = 15;
+                        $in_carbohydrates = 420;
                         // ▼グラフの中身
-                        var pieData = [
-                            {
-                                value: 240,            // 値
-                                color:"#F7464A",       // 色
-                                highlight: "#FF5A5E",  // マウスが載った際の色
-                                label: "タンパク質"        // ラベル
-                            },
-                            {
-                                value: 50,
-                                color: "#41C44E",
-                                highlight: "#6CD173",
-                                label: "脂質"
-                            },
-                            {
-                                value: 100,
-                                color: "#FDB45C",
-                                highlight: "#FFC870",
-                                label: "炭水化物"
-                            },
-                            {
-                                value: 65,
-                                color: "#AA49B8",
-                                highlight: "#C583CF",
-                                label: "ビタミン"
-                            },
-                            {
-                                value: 30,
-                                color: "#4D5360",
-                                highlight: "#616774",
-                                label: "ミネラル"
+                        var barChartData = {
+                                labels: ["タンパク質", "脂質", "炭水化物"],
+                                datasets: [
+                                    {
+                                        fillColor: "rgba(240,128,128,0.6)",    // 塗りつぶし色
+                                        strokeColor: "rgba(240,128,128,0.9)",  // 枠線の色
+                                        highlightFill: "rgba(255,64,64,0.75)",  // マウスが載った際の塗りつぶし色
+                                        highlightStroke: "rgba(255,64,64,1)",   // マウスが載った際の枠線の色
+                                        data: [$in_protein, $in_lipid, $in_carbohydrates]     // 各棒の値(カンマ区切りで指定)
+                                    },
+                                    {
+                                        fillColor: "rgba(151,187,205,0.6)",
+                                        strokeColor: "rgba(151,187,205,0.9)",
+                                        highlightFill: "rgba(64,96,255,0.75)",
+                                        highlightStroke: "rgba(64,96,255,1)",
+                                        data: [$protein, $lipid, $carbohydrates]
+                                    }
+                                ]
+
                             }
 
-                        ];
-                        // ▼上記のグラフを描画するための記述
-                        window.onload = function(){
-                            var ctx = document.getElementById("graph-area").getContext("2d");
-                            window.myPie = new Chart(ctx).Pie(pieData);
-                        };
+                            // ▼上記のグラフを描画するための記述
+                            window.onload = function () {
+                                var ctx = document.getElementById("graph-area").getContext("2d");
+                                window.myBar = new Chart(ctx).Bar(barChartData);
+                            }
                     </script>
                     <table border="5">
                         <tr>
@@ -62,25 +56,15 @@
                         </tr>
                         <tr>
                             <td> タンパク質</td>
-                            <td> 240</td>
+                            <td>{$in_protein}</td>
                         </tr>
                         <tr>
                             <td> 脂質</td>
-                            <td> 50</td>
+                            <td>{$in_protein}</td>
                         </tr>
                         <tr>
                             <td> 炭水化物</td>
-                            <td> 100</td>
-                        </tr>
-                        <tr>
-                            <td> ビタミン</td>
-                            <td> 65</td>
-                        </tr>
-                        <tr>
-                            <td> ミネラル</td>
-                            <td> 30</td>
-                        </tr>
-
+                            <td>{$in_protein}</td>
                 </div>
             </div>
             
