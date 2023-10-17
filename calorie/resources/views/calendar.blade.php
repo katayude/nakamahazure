669ad31ce92636a5b3509be1092c01f12541d18b
skenta114 <s.kentafkm@icloud.com>
2023-10-17 10:31:31 +0900

    カレンダーページのデザイン調整

--- calorie/resources/views/calendar.blade.php
+++ calorie/resources/views/calendar.blade.php
@@ -1,39 +1,55 @@
-<!DOCTYPE html>
-<html>
-<head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <meta charset='utf-8' />
-    <link href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/6.10.0/main.min.css' rel='stylesheet' />
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/6.10.0/main.min.js'></script>
-    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/interaction@6.10.0/main.js'></script>
-</head>
-<body>
-    <div id='calendar'></div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('カレンダー') }}
+        </h2>
+    </x-slot>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+            <meta charset='utf-8' />
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/6.10.0/main.min.js'></script>
+            <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/interaction@6.10.0/main.js'></script>
+        </head>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                plugins: ['interaction'], // Interaction プラグインを有効にする
+        <body>
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg max-w-7xl">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            <div class="container">
+                                <div id='calendar'></div>
+                                <script>
+                                    document.addEventListener('DOMContentLoaded', function() {
+                                        var calendarEl = document.getElementById('calendar');
+                                        var calendar = new FullCalendar.Calendar(calendarEl, {
+                                            plugins: ['interaction'], // Interaction プラグインを有効にする
 
-                // カレンダーの設定...
+                                            // カレンダーの設定...
 
-                dateClick: function(info) {
-                    // クリックした日付を取得
-                    var clickedDate = info.date;
-                    
-                    // ローカルタイムゾーンでフォーマット
-                    var year = clickedDate.getFullYear();
-                    var month = String(clickedDate.getMonth() + 1).padStart(2, '0');
-                    var day = String(clickedDate.getDate()).padStart(2, '0');
-                    var formattedDate = year + '-' + month + '-' + day;
-                    
-                    // ページをリダイレクト
-                    window.location.href = '/dashboard/' + formattedDate;
-                }
-            });
-            calendar.render();
-        });
-    </script>
-</body>
-</html>
+                                            dateClick: function(info) {
+                                                // クリックした日付を取得
+                                                var clickedDate = info.date;
+                                                
+                                                // ローカルタイムゾーンでフォーマット
+                                                var year = clickedDate.getFullYear();
+                                                var month = String(clickedDate.getMonth() + 1).padStart(2, '0');
+                                                var day = String(clickedDate.getDate()).padStart(2, '0');
+                                                var formattedDate = year + '-' + month + '-' + day;
+                                                
+                                                // ページをリダイレクト
+                                                window.location.href = '/dashboard/' + formattedDate;
+                                            }
+                                        });
+                                        calendar.render();
+                                    });
+                                </script>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html>
+</x-app-layout>
