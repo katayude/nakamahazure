a3937d2ed16ef7cae96273d908dc60027143472b
katayude <xXgucchiXx@gmail.com>
2023-10-21 10:46:16 +0900

    foodテーブルにuser_idを追加したので、料理登録の時にuser_idも登録するようにした

--- calorie/app/Models/Food.php
+++ calorie/app/Models/Food.php
@@ -11,6 +11,7 @@ class Food extends Model
 
     protected $fillable = [
         'name', 'ingredient1_id', 'ingredient2_id', 'ingredient3_id', 'ingredient4_id', 'ingredient5_id',
-        'ingredient1_weight', 'ingredient2_weight', 'ingredient3_weight', 'ingredient4_weight', 'ingredient5_weight', 'calorie'
+        'ingredient1_weight', 'ingredient2_weight', 'ingredient3_weight', 'ingredient4_weight', 'ingredient5_weight', 'calorie',
+        'user_id'
     ];
 }
